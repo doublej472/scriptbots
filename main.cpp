09a93276dc71ba47322cb0ce81ae33c63f07576d
Gregory Hopkins <spudnik@ubuntu.(none)>
2012-04-11 18:45:19 -0600

    Added command line arguments to enable headless mode even when OpenGL is available, verbose mode, and for loading an existing world. At the moment, you have to go into the build directory and run the executable directly in order to include the command line arguments, but I will be adding that capability to the autorun.sh file as well.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <stdio.h>
+#include <getopt.h>
 
 #include "config.h"
 #include "World.h"
@@ -37,11 +38,14 @@ using namespace std;
 #if OPENGL
 GLView* GLVIEW = new GLView();
 #endif
-
+bool VERBOSE = false; // Run in verbose mode
+bool HEADLESS = false; // Run without graphics even if OpenGL and GLUT available
 // ---------------------------------------------------------------------------
 // Prototypes:
 int kbhit();
 static inline void loadBar(int x, int n, int r, int w);
+void runHeadless(Base &base);
+void runWithGraphics(int &argc, char** argv, Base &base);
 
 // ---------------------------------------------------------------------------
 int main(int argc, char **argv)
@@ -59,63 +63,53 @@ int main(int argc, char **argv)
 #endif
 	if (conf::WIDTH%conf::CZ!=0 || conf::HEIGHT%conf::CZ!=0)
 		printf("   WARNING: The cell size variable conf::CZ should divide evenly into conf::WIDTH and conf::HEIGHT\n");
-	
-#if OPENGL
-	printf("\nInstructions:\n");
-	printf("   p= pause, d= toggle drawing (for faster computation), f= draw food too, += faster, -= slower\n");
-	printf("   Pan around by holding down right mouse button, and zoom by holding down middle button.\n");
-	printf("\nBot Status Colors: \n");
-	printf("   WHITE: they just ate part of another agent\n");
-	printf("   YELLOW: bot just spiked another bot\n");
-	printf("   GREEN: agent just reproduced\n");
-	printf("   GREY: bot is getting group health bonus\n");
-#else
-	cout << "   Headless Mode - No graphics\n";
-	cout << "      Press any key to save and end simulation\n" << endl;	
-#endif
+
 	cout << "-------------------------------------------------------------------------------" << endl;	
 	
 	srand(time(0));
 
 	Base base;
+	
+	bool loadWorldFromFile;
 
 	// If any argument is passed, just load the file
+	/*
 	if( argc > 1 )
 	{
 		base.loadWorld();
 	}	
+	*/
+	
+	int c;
+	while( (c = getopt(argc, argv, "vhw")) != -1){
+		switch(c){
+			case 'h':
+				HEADLESS = true;
+				break;
+			case 'v':
+				VERBOSE = true;
+				break;
+			case 'w':
+				loadWorldFromFile = true;
+			default:
+				break;
+		}
+	}
 
+	if(loadWorldFromFile)
+		base.loadWorld();
+		
 #if OPENGL
 
-	GLVIEW->setBase(&base);
-
-	//GLUT SETUP
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(30,30);
-	glutInitWindowSize(conf::WWIDTH,conf::WHEIGHT);
-	glutCreateWindow("Scriptbots");
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-	glutDisplayFunc(gl_renderScene);
-	glutIdleFunc(gl_handleIdle);
-	glutReshapeFunc(gl_changeSize);
-
-	glutKeyboardFunc(gl_processNormalKeys);
-	glutMouseFunc(gl_processMouse);
-	glutMotionFunc(gl_processMouseActiveMotion);
-
-	glutMainLoop();
-#else
-	while( !kbhit() )
+	if(HEADLESS)
 	{
-		base.world->update();
-
-		//loadBar(base.world->,  60, 1, 60);
-		
+		runHeadless(base);
 	}
-   	//base.runWorld(12);
-
-	base.saveWorld();
+	else{
+		runWithGraphics(argc, argv, base);
+	}
+#else
+	runHeadless(base);
 
 #endif
 
@@ -179,4 +173,54 @@ int kbhit()
 	}
 
 	return 0;
+}
+// ---------------------------------------------------------------------------
+// Run Scriptbots with graphics
+// --------------------------------------------------------------------------- 
+void runWithGraphics(int &argc, char** argv, Base &base){
+	printf("\nInstructions:\n");
+	printf("   p= pause, d= toggle drawing (for faster computation), f= draw food too, += faster, -= slower\n");
+	printf("   Pan around by holding down right mouse button, and zoom by holding down middle button.\n");
+	printf("\nBot Status Colors: \n");
+	printf("   WHITE: they just ate part of another agent\n");
+	printf("   YELLOW: bot just spiked another bot\n");
+	printf("   GREEN: agent just reproduced\n");
+	printf("   GREY: bot is getting group health bonus\n");
+	
+	GLVIEW->setBase(&base);
+
+	//GLUT SETUP
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(30,30);
+	glutInitWindowSize(conf::WWIDTH,conf::WHEIGHT);
+	glutCreateWindow("Scriptbots");
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	glutDisplayFunc(gl_renderScene);
+	glutIdleFunc(gl_handleIdle);
+	glutReshapeFunc(gl_changeSize);
+
+	glutKeyboardFunc(gl_processNormalKeys);
+	glutMouseFunc(gl_processMouse);
+	glutMotionFunc(gl_processMouseActiveMotion);
+
+	glutMainLoop();
+}
+// ---------------------------------------------------------------------------
+// Run Scriptbots headless
+// --------------------------------------------------------------------------- 
+void runHeadless(Base &base){
+	cout << "   Headless Mode - No graphics\n";
+	cout << "      Press any key to save and end simulation\n" << endl;	
+	
+	while( !kbhit() )
+	{
+		base.world->update();
+
+		//loadBar(base.world->,  60, 1, 60);
+		
+	}
+   	//base.runWorld(12);
+
+	base.saveWorld();
 }
