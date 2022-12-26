31af69ed4bb9f6cdd2f5c0ee5e471f979dc3b023
Jonathan Frederick <doublej472@gmail.com>
2022-12-25 23:21:46 -0800

    Fixup alloc alignment

--- src/Base.c
+++ src/Base.c
@@ -60,7 +60,7 @@ void base_loadworld(struct Base *base) {
 
   printf("Reading %ld brains...\n", size);
   for (int i = 0; i < base->world->agents.size; i++) {
-    base->world->agents.agents[i].brain = malloc(sizeof(struct AVXBrain));
+    base->world->agents.agents[i].brain = aligned_alloc(32, sizeof(struct AVXBrain));
     fread(base->world->agents.agents[i].brain, sizeof(struct AVXBrain), 1, f);
   }
 
